afa902d152cc03211fb8cd423ced3683777f156b
Shannen Reyes <98503879+kishannen@users.noreply.github.com>
2024-01-30 14:23:57 +0800

    App.jsx

---
+++ prelimexam/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Biblequiz from './components/biblequiz'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1> The Bible Quiz </h1>
+        <Biblequiz />
+      </div>
+     
+    </>
+  )
+}
+
+export default App
